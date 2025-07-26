bfd05a71d779b2151ca5ed9f90108bceee33010c
Whiteout148 <rinattemeev@gmail.com>
2025-07-26 20:28:39 +0300

    file for school
    скрипт первое дз

---
+++ dz1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace imSenior
+{
+    internal class dz1
+    {
+        static void Main(string[] args)
+        {
+            string name = "Алексей";
+            string surname = "Черненко";
+            bool isEmployed = false;
+
+            int age = 15;
+
+            string countryOfLifeName = "Беларусь";
+            string cityofLifeName = "минск";
+
+            float degreesInOutside = 35.6f;
+
+            char group = 'A';
+
+            int classOfTheSchool = 9;
+            string favouriteGame = "cs2Surf";
+        }
+    }
+}
