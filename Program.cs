68a9971ff913d34e1c2f61534156666a07b85d44
Whiteout148 <rinattemeev@gmail.com>
2025-07-27 20:27:33 +0300

    хз че сказать
    тоже самое

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,20 +11,21 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            int gold;
-            int amethysts;
-            int amethystsPrice = 10;
+            string name;
+            string surname;
+            string favouriteDrink;
+            int age;
 
-            Console.WriteLine("Введите количество золото: ");
-            gold = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Введите количество аметистов которые хотите купить: ");
-            amethysts = Convert.ToInt32(Console.ReadLine());
-            int resultPriсe = amethysts * amethystsPrice;
-            gold -= resultPriсe;
+            Console.WriteLine("Введите имя: ");
+            name = Console.ReadLine();
+            Console.WriteLine("Введите фамилию: ");
+            surname = Console.ReadLine();
+            Console.WriteLine("Введите возраст: ");
+            age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите любимый напиток: ");
+            favouriteDrink = Console.ReadLine();
 
-            Console.WriteLine($"Вы купили {amethysts} аметистов по цене {resultPriсe}. у вас осталось {gold} золото");
-
-            Console.ReadKey();
+            Console.WriteLine($"Вы {name} у вас фамилия {surname} вам {age} лет и у вас любимый напиток это {favouriteDrink}");
         }
     }
 }
