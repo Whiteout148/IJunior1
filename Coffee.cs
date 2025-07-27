989b6da139505287f719de9f1e63b34640b26cb9
Whiteout148 <rinattemeev@gmail.com>
2025-07-27 21:48:01 +0300

    Add files via upload

---
+++ Coffee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @switch
+{
+    internal class Coffee
+    {
+        static void Main(string[] args)
+        {
+            string name = "Буянов";
+            string surname = "Владимир";
+
+            string tea = "капучино";
+            string coffee = "индийский чай";
+
+            Console.WriteLine($"Имя: {name}\nФамилия: {surname}\nБудете пить {tea}? или же кофе {coffee}");
+
+            name = "Владимир";
+            surname = "Буянов";
+
+            tea = "Индийский чай";
+            coffee = "капучино";
+
+            Console.WriteLine($"\nИмя: {name}\nФамилия: {surname}\nБудете пить {tea}? или же кофе {coffee}");
+
+            Console.ReadKey();
+        }
+    }
+}
