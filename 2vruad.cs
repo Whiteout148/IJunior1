e0a2e22cf90c0c861339a1233a5753803a182bcd
Whiteout148 <rinattemeev@gmail.com>
2025-07-27 21:30:35 +0300

    альбом
    домашка

---
+++ 2vruad.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_vryad
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int album = 52;
+            int row = 3;
+
+            int rowNumber = album / row;
+            int remainder = album % 3;
+
+            Console.WriteLine($"Количество рядов {rowNumber} остаток фотографий {remainder}");
+
+            Console.ReadLine();
+        }
+    }
+}
