e0f2e788bff978e8e0cafa1ece8900d49767b9a8
Whiteout148 <rinattemeev@gmail.com>
2025-07-28 15:59:59 +0300

    Add files via upload

---
+++ WhileRepeats.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhilesPractice1
+{
+    internal class WhileRepeats
+    {
+        static void Main(string[] args)
+        {
+            int repeatsNumber;
+            string userMessage;
+
+            Console.Write("Введите сообщение которое хотите повторить: ");
+            userMessage = Console.ReadLine();
+            Console.Write("\nВведите количество повторов: ");
+            repeatsNumber = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < repeatsNumber; i++)
+            {
+                Console.WriteLine("\n" + userMessage);
+            }
+        }
+    }
+}
