18ed64feb8525b492aa410241dcbdd3b1910ac49
Whiteout148 <rinattemeev@gmail.com>
2025-07-28 15:09:42 +0300

    Add files via upload

--- ProgramPatients.cs
+++ ProgramPatients.cs
@@ -7,17 +7,18 @@ namespace XDproject
         static void Main()
         {
             int patient;
-            int patientReceptionTime = 10;
-            int hour = 60;
+            int patientReceptionTimeWithMinutes = 10;
+            int oneHour = patientReceptionTimeWithMinutes * 6;
 
             Console.WriteLine("Введите количество пациентов");
             patient = Convert.ToInt32(Console.ReadLine());
 
-            int allPatientsReceptionTime = patient * patientReceptionTime;
-            int allHours = allPatientsReceptionTime / hour;
-            int minutes = allPatientsReceptionTime % hour;
+            int allPatientsReceptionTime = patient * patientReceptionTimeWithMinutes;
+            int HoursRemaining = allPatientsReceptionTime / oneHour;
+            int minutesRemaining = allPatientsReceptionTime % oneHour;
 
-            Console.WriteLine($"Время прибытие одного пациента: {patientReceptionTime}\nвам стоят в очереди {allHours} часов {minutes} минут.");
+            Console.WriteLine($"Время прибытие одного пациента: {patientReceptionTimeWithMinutes}" +
+                $"\nвам стоят в очереди {HoursRemaining} часов {minutesRemaining} минут.");
 
             Console.ReadLine();
         }
