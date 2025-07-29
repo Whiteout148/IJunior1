a55c7cdd9caa345d19bad8e2d60006f16adb7b0a
Whiteout148 <rinattemeev@gmail.com>
2025-07-29 19:31:29 +0300

    Add files via upload

--- practiceSwitch.cs
+++ practiceSwitch.cs
@@ -5,72 +5,28 @@ namespace KurwaTSIKLI
 {
     internal class practiceSwitch
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            const string CommandForGetMail = "1";
-            const string CommandForGetPhoneNumber = "2";
-            const string CommandForGetRandomCode = "3";
-            const string CommandForClearConsol = "4";
-            const string CommandForExit = "5";
-
-            string userInput;
-            string phoneNumber = "7034340583";
-            string mail = "vladimirkovsky@gmail.com";
-
-            int randomAccessCode;
-            int initialNumberInCode = 100;
-            int lastNumberInCode = 998;
-
             Random random = new Random();
+            
+            int randomNumber = random.Next(1, 101);
+            int firstMultiplicationNumber = 3;
+            int secondMultiplicationNumber = 5;
+            int sum = 0;
 
-            bool isWorking = true;
-
-            while (isWorking)
+            for (int i = 1; i <= randomNumber; i++)
             {
-                Console.WriteLine("\nСлужба поддержки нашей программы, выберите номер команды для операции: ");
-                Console.WriteLine($"\n{CommandForGetMail} - Получить доступ к электронной почте");
-                Console.WriteLine($"\n{CommandForGetPhoneNumber} - Получить доступ к номеру телефона");
-                Console.WriteLine($"\n{CommandForGetRandomCode} - Сгенерировать рандомный код доступа");
-                Console.WriteLine($"\n{CommandForClearConsol} - Очистить консоль");
-                Console.WriteLine($"\n{CommandForExit} - Выход из программы");
-
-                userInput = Console.ReadLine();
-
-                switch(userInput)
+                if (i % firstMultiplicationNumber == 0 || i % secondMultiplicationNumber == 0)
                 {
-                    case CommandForGetMail:
-                        Console.WriteLine($"Ваша электронная почта: {mail}");
-                        break;
-
-                    case CommandForGetPhoneNumber:
-                        Console.WriteLine($"Ваш номер телефона: {phoneNumber}");
-                        break;
-
-                    case CommandForGetRandomCode:
-                        randomAccessCode = random.Next(initialNumberInCode, lastNumberInCode + 1);
-
-                        Console.WriteLine($"Ваш код доступа: {randomAccessCode}");
-                        break;
-
-                    case CommandForClearConsol:
-                        Console.Clear();
-                        break;
-
-                    case CommandForExit:
-                        Console.Clear();
-                        Console.WriteLine("Вы вышли из программы");
-
-                        isWorking = false;
-                        break;
-
-                    default:
-                        Console.WriteLine("Неверный ввод команды!");
-                        break;
+                    Console.WriteLine($"Добавлено число: {i}");
+                    sum += i;
                 }
-
-                Console.ReadKey();
-                Console.Clear();
             }
+
+            Console.WriteLine($"рандомное число: {randomNumber}");
+            Console.WriteLine($"сумма чисел кратных {firstMultiplicationNumber} или {secondMultiplicationNumber}: {sum}");
+
+            Console.WriteLine("");
         }
     }
 }
