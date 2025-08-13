982c45d0e006d5f2c2ad2efabfad4678f4a1d157
Whiteout148 <rinattemeev@gmail.com>
2025-08-13 19:43:09 +0300

    Add files via upload

--- ProgramPatients.cs
+++ ProgramPatients.cs
@@ -8,23 +8,24 @@ namespace XDproject
         {
             Random random = new Random();
 
-            int[,] array = new int[10, 10];
+            int[,] numbers = new int[10, 10];
 
-            int maxNumber = 0;
+            int maxNumber = int.MinValue;
             int maxRandomNumber = 1000;
+            int resetMaxNumber = 0;
 
-            for (int i = 0; i < array.GetLength(0); i++)
+            for (int i = 0; i < numbers.GetLength(0); i++)
             {
-                for (int j = 0; j < array.GetLength(1); j++)
+                for (int j = 0; j < numbers.GetLength(1); j++)
                 {
-                    array[i, j] = random.Next(0, maxRandomNumber + 1);
+                    numbers[i, j] = random.Next(0, maxRandomNumber + 1);
 
-                    if (maxNumber < array[i, j])
+                    if (maxNumber < numbers[i, j])
                     {
-                        maxNumber = array[i, j];
+                        maxNumber = numbers[i, j];
                     }
 
-                    Console.Write(array[i, j] + " | ");
+                    Console.Write(numbers[i, j] + " | ");
                 }
 
                 Console.WriteLine();
@@ -32,20 +33,23 @@ namespace XDproject
 
             Console.WriteLine("\nМаксимальное число: " + maxNumber + "\n");
 
-            for (int i = 0;i < array.GetLength(0); i++)
+            for (int i = 0;i < numbers.GetLength(0); i++)
             {
-                for(int j = 0;j < array.GetLength(1); j++)
+                for(int j = 0;j < numbers.GetLength(1); j++)
                 {
-                    if (array[i, j] == maxNumber)
+                    if (numbers[i, j] == maxNumber)
                     {
-                        array[i, j] = 0;
+                        maxNumber = resetMaxNumber;
+                        numbers[i, j] = maxNumber;
                     }
 
-                    Console.Write(array[i, j] + " | ");
+                    Console.Write(numbers[i, j] + " | ");
                 }
 
                 Console.WriteLine();
             }
+
+            Console.ReadKey();
         }
     }
 }
