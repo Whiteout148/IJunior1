ad6698b11a6658b5ff4edb684720662aa313213f
Whiteout148 <rinattemeev@gmail.com>
2025-08-16 17:12:06 +0300

    Add files via upload

--- practiceSwitch.cs
+++ practiceSwitch.cs
@@ -1,42 +1,83 @@
 ﻿using System;
+using System.Net.Http.Headers;
+using System.Threading;
 
 class Program
 {
     static void Main()
     {
-        int[,] array = new int[3, 3];
-        Random random = new Random();
+        const int CommandForAddNumber = 1;
+        const int CommandForCheckSum = 2;
+        const int CommandForExit = 3;
 
-        int maxRandomNumber = 10;
-        int sumOfShelf = 0;
-        int productOfColumn = 1;
+        int sum = 0;
 
-        int indexOfShelf = 1;
-        int indexOfColumn = 0;
+        int userNumber;
+        int userNumbersSum = 0;
 
-        for (int i = 0; i < array.GetLength(0); i++)
+        int[] numbers = new int[1];
+        int[] tempNumbers;
+
+        bool isWork = true;
+
+        int oneSecondWithMs = 3000;
+
+        while (isWork)
         {
-            for (int j = 0; j < array.GetLength(1); j++)
+            Console.WriteLine("** Программа для вычисление всех написанных чисел **\n");
+            Console.WriteLine($"Команда для добавление числа в сумму: {CommandForAddNumber}");
+            Console.WriteLine($"Команда для показа суммы всех чисел: {CommandForCheckSum}");
+            Console.WriteLine($"Команда для выхода из программы: {CommandForExit}");
+
+            switch(Convert.ToInt32(Console.ReadLine()))
             {
-                array[i, j] = random.Next(0, maxRandomNumber + 1);
-                Console.Write(array[i, j] + " | ");
+                case CommandForAddNumber:
+                    Console.Write("Введите число которое хотите добавить: ");
+                    userNumber = Convert.ToInt32(Console.ReadLine());
+                    userNumbersSum++;
+                    tempNumbers = new int[userNumbersSum];
+
+                    for (int i = 0; i < numbers.Length; i++)
+                    {
+                        tempNumbers[i] = numbers[i];
+                    }
+
+                    tempNumbers[tempNumbers.Length - 1] = userNumber;
+                    numbers = tempNumbers;
+
+                    Console.WriteLine("Ваши числа: ");
+
+                    for (int i = 0; i < numbers.Length; i++)
+                    {
+                        Console.WriteLine(numbers[i]);
+                    }
+                    break;
+
+                case CommandForCheckSum:
+                    Console.WriteLine("Ваши числа: \n");
+
+                    for (int i = 0; i < numbers.Length; i++)
+                    {
+                        Console.WriteLine(numbers[i]);
+                        sum += numbers[i];
+                    }
+
+                    Console.WriteLine("\nСумма всех чисел: " + sum);
+                    break;
+
+                case CommandForExit:
+                    Console.WriteLine("Выход из программы.");
+                    Thread.Sleep(oneSecondWithMs);
+                    isWork = false;
+                    break;
+
+                default:
+                    Console.WriteLine("Нету такой команды.");
+                    break;
             }
 
-            Console.WriteLine();
+            Console.ReadKey();
+            Console.Clear();
         }
-
-        for (int i = 0; i < array.GetLength(1); i++)
-        {
-            sumOfShelf += array[indexOfShelf, i];
-        }
-
-        for (int i = 0; i < array.GetLength(0); i++)
-        {
-            productOfColumn *= array[i, indexOfColumn];
-        }
-
-        Console.WriteLine("Сумма второй строки: " + sumOfShelf);
-        Console.WriteLine("Произведение первого столбца: " + productOfColumn);
-        Console.ReadKey();
     }
 }
