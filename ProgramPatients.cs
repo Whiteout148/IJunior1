701191686772bcd65a71e219ebd69a2525f92251
Whiteout148 <rinattemeev@gmail.com>
2025-10-13 22:14:06 +0300

    Add files via upload

--- ProgramPatients.cs
+++ ProgramPatients.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Runtime.InteropServices;
-using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading;
 
 namespace XDproject
