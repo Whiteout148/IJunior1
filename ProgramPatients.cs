d850e7d72014087eecad5f65e6243c4613504491
Whiteout148 <rinattemeev@gmail.com>
2025-11-02 21:59:38 +0300

    Add files via upload

--- ProgramPatients.cs
+++ ProgramPatients.cs
@@ -9,100 +9,6 @@ namespace XDproject
         static void Main()
         {
             BattleField battleField = new BattleField();
-
-            battleField.Work();
-        }
-    }
-
-    class BattleField
-    {
-        private List<Fighter> _firstPlatoon = new List<Fighter>();
-        private List<Fighter> _secondPlatoon = new List<Fighter>();
-
-        public BattleField()
-        {
-            _firstPlatoon.Add(new Fighter("Леха", 100, 20, 30));
-            _firstPlatoon.Add(new Sniper(2, "Максим", 90, 20, 50));
-            _firstPlatoon.Add(new GrenadeLauncher(3, "Иван", 100, 10, 40));
-            _firstPlatoon.Add(new MachineGunner(5, "Антон", 120, 20, 10));
-            _firstPlatoon.Add(new Sniper(3, "Второй Максим", 70, 40, 50));
-
-            _secondPlatoon.Add(new Fighter("Вова", 100, 30, 20));
-            _secondPlatoon.Add(new Sniper(3, "Серега", 120, 10, 10));
-            _secondPlatoon.Add(new GrenadeLauncher(3, "Андрей", 100, 20, 10));
-            _secondPlatoon.Add(new MachineGunner(3, "Михаил", 100, 10, 30));
-            _secondPlatoon.Add(new MachineGunner(5, "Второй Серега", 60, 20, 30));
-        }
-
-        public void Work()
-        {
-            ShowPlatoon(_firstPlatoon);
-            Console.WriteLine();
-            ShowPlatoon(_secondPlatoon);
-            Console.WriteLine("Нажмите любую кнопку чтобы начать бой между взводами.");
-            Console.ReadKey();
-            Console.WriteLine();
-
-            while (GetFightersHealth(_firstPlatoon) > 0 && GetFightersHealth(_secondPlatoon) > 0)
-            {
-                AttackPlatoon(_firstPlatoon, _secondPlatoon, "Атакует первый взвод.");
-                Console.WriteLine();
-                AttackPlatoon(_secondPlatoon, _firstPlatoon, "Атакует второй взвод.");
-
-                Console.Clear();
-            }
-
-            ShowResults();
-        }
-
-        private int GetFightersHealth(List<Fighter> platoon)
-        {
-            int resultHealth = 0;
-
-            for (int i = 0; i < platoon.Count; i++)
-            {
-                resultHealth += platoon[i].Health;
-            }
-
-            return resultHealth;
-        }
-
-        private void ShowResults()
-        {
-            if (GetFightersHealth(_firstPlatoon) > 0 && GetFightersHealth(_secondPlatoon) <= 0)
-            {
-                Console.WriteLine("Выиграл первый взвод.");
-            }
-            else if (GetFightersHealth(_secondPlatoon) > 0 && GetFightersHealth(_firstPlatoon) <= 0)
-            {
-                Console.WriteLine("Выиграл второй взвод");
-            }
-            else
-            {
-                Console.WriteLine("Ничья.");
-            }
-        }
-
-        private void ShowPlatoon(List<Fighter> platoon)
-        {
-            for (int i = 0; i < platoon.Count; i++)
-            {
-                platoon[i].ShowInfo();
-            }
-        }
-
-        private void AttackPlatoon(List<Fighter> platoon, List<Fighter> platoonToAttack, string message)
-        {
-            int halfSecondWithMs = 500;
-
-            Console.WriteLine(message);
-
-            for (int i = 0; i < platoon.Count; i++)
-            {
-                platoon[i].ShowAttackInfo();
-                platoon[i].Attack(platoonToAttack);
-                Thread.Sleep(halfSecondWithMs);
-            }
         }
     }
 
@@ -111,47 +17,85 @@ namespace XDproject
         void TakeDamage(int damage);
     }
 
-    class Fighter : IDamageable
+    class BattleField
     {
-        protected string _name;
-        protected int _damage;
-        protected int _armor;
+        private Platoon _firstPlatoon = new Platoon();
+        private Platoon _secondPlatoon = new Platoon();
 
-        public Fighter(string name, int health, int damage, int armor)
+        private void AddFightersToPlatoon()
         {
-            Health = health;
-            _name = name;
-            _damage = damage;
-            _armor = armor;
+            _firstPlatoon.AddFighter(new Fighter("Х1", 100, 20, 30));
+            _firstPlatoon.AddFighter(new Sniper(2 ,"Х2", 90, 30, 60));
+            _firstPlatoon.AddFighter(new GrenadeLauncher(4 ,"Х3", 110, 20, 30));
+            _firstPlatoon.AddFighter(new MachineGunner(3, "Х4", 100, 20, 50));
+            _firstPlatoon.AddFighter(new GrenadeLauncher(5, "Х5", 100, 30, 30));
+
+            _secondPlatoon.AddFighter(new Fighter("Д1", 100, 20, 40));
+            _secondPlatoon.AddFighter(new Sniper(3 ,"Д2", 100, 20, 50));
+            _secondPlatoon.AddFighter(new GrenadeLauncher(2,"Д3", 120, 20, 50));
+            _secondPlatoon.AddFighter(new MachineGunner(3,"Д4", 110, 30, 50));
+            _secondPlatoon.AddFighter(new Sniper(4,"Д5", 110, 40, 60));
+        }
+    }
+
+    class Platoon
+    {
+        private List<Fighter> _fighters = new List<Fighter>();
+        public int FighterCount { get { return _fighters.Count; } }
+
+        public void ShowFighters()
+        {
+            for (int i = 0; i < _fighters.Count; i++)
+            {
+                _fighters[i].ShowInfo();
+                Console.WriteLine();
+            }
         }
 
+        public void AddFighter(Fighter fighter)
+        {
+            _fighters.Add(fighter);
+        }
+
+        public int GetFightersHealth()
+        {
+            int resultHealth = 0;
+
+            for (int i = 0; i < _fighters.Count; i++)
+            {
+                resultHealth += _fighters[i].Health;
+            }
+
+            return resultHealth;
+        }
+    }
+
+    class Fighter 
+    {
+        protected int Damage;
+        protected int Armor;
+
+        public Fighter(string name ,int health, int damage, int armor)
+        {
+            Name = name;
+            Health = health;
+            Damage = damage;
+            Armor = armor;
+        }
+
+        public string Name { get; protected set; }
         public int Health { get; protected set; }
 
         public void ShowInfo()
         {
-            Console.WriteLine($"Имя {_name} Здоровье {Health} Урон {_damage} Броня {_armor}");
-            Console.Write("Тип: ");
+            Console.WriteLine($"Имя {Name} Здоровье {Health} Урон {Damage} Броня {Armor}");
+            Console.WriteLine("Тип:");
             ShowType();
         }
 
-        public void TakeDamage(int damage)
+        public virtual void ShowType()
         {
-            Health -= (damage * UserUtils.MaxPercent / (UserUtils.MaxPercent + _armor));
-        }
-
-        public void ShowAttackInfo()
-        {
-            Console.WriteLine($"Боец {_name} атакует.");
-        }
-
-        public virtual void Attack(List<Fighter> target)
-        {
-            target[UserUtils.GetRandomNumber(0, target.Count - 1)].TakeDamage(_damage);
-        }
-
-        protected virtual void ShowType()
-        {
-            Console.WriteLine("Обычный.");
+            Console.WriteLine("Обычный");
         }
     }
 
@@ -159,85 +103,55 @@ namespace XDproject
     {
         private int _damageCounter;
 
-        public Sniper(int damageCounter, string name, int health, int damage, int armor) : base(name, health, damage, armor)
+        public Sniper(int damageCounter, string name, int health, int damage, int armor) : base(name ,health, damage, armor)
         {
             _damageCounter = damageCounter;
+
+            damage *= _damageCounter;
         }
 
-        public override void Attack(List<Fighter> target)
+        public override void ShowType()
         {
-            int resultDamage = _damage * _damageCounter;
-
-            target[UserUtils.GetRandomNumber(0, target.Count - 1)].TakeDamage(resultDamage);
-        }
-
-        protected override void ShowType()
-        {
-            Console.WriteLine("Снайпер.");
-            Console.WriteLine("Множитель урона: " + _damageCounter);
+            Console.WriteLine($"Снайпер, множитель урона: {_damageCounter}");
         }
     }
 
     class GrenadeLauncher : Fighter
     {
-        private List<int> _AttackedTargetsIndex = new List<int>();
-        private int _targetsCount;
+        private int _targets;
 
-        public GrenadeLauncher(int targetsCount, string name, int health, int damage, int armor) : base(name, health, damage, armor)
+        public GrenadeLauncher(int targets, string name, int health, int damage, int armor) : base(name, health, damage, armor)
         {
-            _targetsCount = targetsCount;
+            _targets = targets;
         }
 
-        public override void Attack(List<Fighter> target)
+        public override void ShowType()
         {
-            for (int i = 0; i < _targetsCount; i++)
-            {
-                int targetIndex = UserUtils.GetRandomNumber(0, target.Count - 1);
-                target[targetIndex].TakeDamage(_damage);
-                _AttackedTargetsIndex.Add(targetIndex);
-            }
-
-            _AttackedTargetsIndex.Clear();
-        }
-
-        protected override void ShowType()
-        {
-            Console.WriteLine("Панзер гренадер.");
-            Console.WriteLine("Количество врагов на атаку: " + _targetsCount);
+            Console.WriteLine("Гранатометчик");
+            Console.WriteLine($"Количество противников на атаку: {_targets}");
         }
     }
 
     class MachineGunner : Fighter
     {
-        private List<int> _AttackedTargetsIndex = new List<int>();
-        private int _targetsCount;
+        private int _targets;
 
-        public MachineGunner(int targetsCount, string name, int health, int damage, int armor) : base(name, health, damage, armor)
+        public MachineGunner(int targets, string name, int health, int damage, int armor) : base(name, health, damage, armor)
         {
-            _targetsCount = targetsCount;
+            _targets = targets;
         }
 
-        public override void Attack(List<Fighter> target)
+        public override void ShowType()
         {
-            for (int i = 0; i < _targetsCount; i++)
-            {
-                target[UserUtils.GetRandomNumber(0, target.Count - 1)].TakeDamage(_damage);
-            }
-
-            _AttackedTargetsIndex.Clear();
-        }
-
-        protected override void ShowType()
-        {
-            Console.WriteLine("Пулеметчик.");
-            Console.WriteLine("Количество врагов на атаку: " + _targetsCount);
+            Console.WriteLine("Пулеметчик");
+            Console.WriteLine($"Количество противников на атаку: {_targets}");
         }
     }
 
     static class UserUtils
     {
-        public const int MaxPercent = 100;
         private static Random s_random = new Random();
+        public static int MaxPercent { get; private set; } = 100;
 
         public static int GetRandomNumber(int min, int max)
         {
