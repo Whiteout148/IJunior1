0f09d80ef1773f5d4626e344f7a01064a3f35c7f
Whiteout148 <rinattemeev@gmail.com>
2025-11-14 00:10:40 +0300

    Add files via upload

--- ProgramPatients.cs
+++ ProgramPatients.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Remoting.Messaging;
+using System.Security.Policy;
 using System.Threading;
 
 namespace XDproject
@@ -8,247 +11,196 @@ namespace XDproject
     {
         static void Main()
         {
-            BattleField battleField = new BattleField();
+            Aquarium aquarium = new Aquarium();
 
-            battleField.Fight();
+            aquarium.Work();
         }
     }
 
-    interface IDamageable
+    class Aquarium
     {
-        void TakeDamage(int damage);
-    }
+        private List<Fish> _fisces = new List<Fish>();
 
-    class BattleField
-    {
-        private Platoon _firstPlatoon = new Platoon();
-        private Platoon _secondPlatoon = new Platoon();
-
-        public BattleField()
+        public void Work()
         {
-            _firstPlatoon.AddFighter(new Fighter("Х1", 100, 40, 30));
-            _firstPlatoon.AddFighter(new Sniper(2, "Х2", 90, 30, 60));
-            _firstPlatoon.AddFighter(new GrenadeLauncher(5, "Х3", 110, 40, 30));
-            _firstPlatoon.AddFighter(new MachineGunner(6, "Х4", 100, 50, 50));
-            _firstPlatoon.AddFighter(new GrenadeLauncher(5, "Х5", 100, 30, 30));
+            const string CommandAddFish = "1";
+            const string CommandTakeFish = "2";
+            const string CommandExit = "3";
 
-            _secondPlatoon.AddFighter(new Fighter("Д1", 100, 20, 40));
-            _secondPlatoon.AddFighter(new Sniper(2, "Д2", 90, 30, 50));
-            _secondPlatoon.AddFighter(new GrenadeLauncher(2, "Д3", 120, 20, 50));
-            _secondPlatoon.AddFighter(new MachineGunner(2, "Д4", 110, 30, 50));
-            _secondPlatoon.AddFighter(new Sniper(2, "Д5", 90, 90, 60));
+            bool isWork = true;
+
+            while (isWork)
+            {
+                Console.WriteLine("Рыбы в аквариуме: ");
+                Console.WriteLine();
+                ShowFisces();
+                Console.WriteLine();
+                Console.WriteLine("Типы команд:");
+                Console.WriteLine($"Добавить рыбу: {CommandAddFish}");
+                Console.WriteLine($"Забрать рыбу: {CommandTakeFish}");
+                Console.WriteLine($"Выход: {CommandExit}");
+                Living();
+
+                switch (Console.ReadLine())
+                {
+                    case CommandAddFish:
+                        AddFish();
+                        break;
+
+                    case CommandTakeFish:
+                        TakeFish();
+                        break;
+
+                    case CommandExit:
+                        isWork = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Нету такой команды.");
+                        break;
+                }
+
+                Console.ReadKey();
+                Console.Clear();
+            }
         }
 
-        public void Fight()
+        private void Living()
         {
-            _firstPlatoon.ShowFighters();
-            Console.WriteLine();
-            _secondPlatoon.ShowFighters();
-            Console.WriteLine("Нажмите любую клавишу чтобы начать бой.");
-            Console.ReadKey();
-
-            while (_firstPlatoon.GetLivingFighters() > 0 && _secondPlatoon.GetLivingFighters() > 0)
+            for (int i = 0; i < _fisces.Count; i++)
             {
-                _firstPlatoon.Attack(_secondPlatoon.FightersToDamage);
-                _secondPlatoon.Attack(_firstPlatoon.FightersToDamage);
-            }
+                _fisces[i].GrowOld();
 
-            ShowResults();
+                if (_fisces[i].IsDie())
+                {
+                    Console.WriteLine($"рыба: {_fisces[i].Name} умерла.");
+                    _fisces.RemoveAt(i);
+                }
+            }
         }
 
-        private void ShowResults()
+        private void ShowFisces()
         {
-            if (_firstPlatoon.GetLivingFighters() > 0 && _secondPlatoon.GetLivingFighters() <= 0)
+            for (int i = 0; i < _fisces.Count; i++)
             {
-                Console.WriteLine("Первый взвод победил.");
+                _fisces[i].ShowInfo();
             }
-            else if (_secondPlatoon.GetLivingFighters() > 0 && _firstPlatoon.GetLivingFighters() <= 0)
+        }
+
+        private void AddFish()
+        {
+            string userName = GetUserMessage("Введите название рыбы:");
+
+            if (IsContainsFish(userName))
             {
-                Console.WriteLine("Второй взвод победил.");
+                Console.WriteLine("Такая рыба уже есть, выберите другое имя.");
             }
             else
             {
-                Console.WriteLine("Ничья");
-            }
-        }
-    }
+                string userAge = GetUserMessage("Введите возраст рыбы:");
 
-    class Platoon
-    {
-        private List<Fighter> _fighters = new List<Fighter>();
-        public List<IDamageable> FightersToDamage => new List<IDamageable>(_fighters);
-
-        public void Attack(List<IDamageable> targets)
-        {
-            for (int i = 0; i < _fighters.Count; i++)
-            {
-                _fighters[i].Attack(targets);
-            }
-        }
-
-        public void ShowFighters()
-        {
-            for (int i = 0; i < _fighters.Count; i++)
-            {
-                _fighters[i].ShowInfo();
-                Console.WriteLine();
-            }
-        }
-
-        public void AddFighter(Fighter fighter)
-        {
-            _fighters.Add(fighter);
-        }
-
-        public int GetLivingFighters()
-        {
-            int fightersCount = 0;
-
-            for (int i = 0; i < _fighters.Count; i++)
-            {
-                if (_fighters[i].Health > 0)
+                if (TryGetAge(userAge, out int resultAge))
                 {
-                    fightersCount++;
+                    _fisces.Add(new Fish(resultAge, userName));
+                    Console.WriteLine("Рыба добавлена.");
+                }
+            }
+        }
+
+        private void TakeFish()
+        {
+            string userName = GetUserMessage("Введите имя рыбы которую хотите забрать:");
+
+            if (IsContainsFish(userName))
+            {
+                _fisces.RemoveAt(GetIndexWithName(userName));
+                Console.WriteLine("Рыба удалена.");
+            }
+            else
+            {
+                Console.WriteLine("Такой рыбы нету!");
+            }
+        }
+
+        private string GetUserMessage(string message)
+        {
+            Console.WriteLine(message);
+            return Console.ReadLine();
+        }
+
+        private bool TryGetAge(string userAge, out int age)
+        {
+            if (int.TryParse(userAge, out age))
+            {
+                if (age <= 0)
+                {
+                    Console.WriteLine("Введите такое количество жизней чтобы рыба не умерла в первую же итерацию!");
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Введите число!");
+            }
+
+            return false;
+        }
+
+        private int GetIndexWithName(string userName)
+        {
+            for (int i = 0; i < _fisces.Count; i++)
+            {
+                if (_fisces[i].Name == userName)
+                {
+                    return i;
                 }
             }
 
-            return fightersCount;
+            return -1;
+        }
+
+        private bool IsContainsFish(string userName)
+        {
+            for (int i = 0; i < _fisces.Count; i++)
+            {
+                if (_fisces[i].Name == userName)
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
     }
 
-    class Fighter : IDamageable
+    class Fish
     {
-        protected int Damage;
-        protected int Armor;
+        private int _health;
 
-        public Fighter(string name, int health, int damage, int armor)
+        public Fish(int health, string name)
         {
+            _health = health;
             Name = name;
-            Health = health;
-            Damage = damage;
-            Armor = armor;
         }
 
-        public string Name { get; protected set; }
-        public int Health { get; protected set; }
+        public string Name { get; private set; }
+
+        public void GrowOld()
+        {
+            _health--;
+        }
+
+        public bool IsDie()
+        {
+            return _health < 0;
+        }
 
         public void ShowInfo()
         {
-            Console.WriteLine($"Имя {Name} Здоровье {Health} Урон {Damage} Броня {Armor}");
-            Console.WriteLine("Тип:");
-            ShowType();
-        }
-
-        public virtual void TakeDamage(int damage)
-        {
-            int resultDamage = damage * UserUtils.MaxPercent / (UserUtils.MaxPercent + Armor);
-
-            Health -= resultDamage;
-
-            if (Health < 0)
-                Health = 0;
-        }
-
-        public virtual void Attack(List<IDamageable> targets)
-        {
-            Console.WriteLine($"Боец {Name} атакует.");
-            ShowType();
-
-            targets[UserUtils.GetRandomNumber(0, targets.Count)].TakeDamage(Damage);
-        }
-
-        public virtual void ShowType()
-        {
-            Console.WriteLine("Обычный");
-        }
-    }
-
-    class Sniper : Fighter
-    {
-        private int _damageFactor;
-
-        public Sniper(int damageFactor, string name, int health, int damage, int armor) : base(name, health, damage, armor)
-        {
-            _damageFactor = damageFactor;
-        }
-
-        public override void Attack(List<IDamageable> targets)
-        {
-            int resultDamage = Damage * _damageFactor;
-
-            targets[UserUtils.GetRandomNumber(0, targets.Count)].TakeDamage(resultDamage);
-        }
-
-        public override void ShowType()
-        {
-            Console.WriteLine($"Снайпер, множитель урона: {_damageFactor}");
-        }
-    }
-
-    class GrenadeLauncher : Fighter
-    {
-        private int _targets;
-
-        public GrenadeLauncher(int targets, string name, int health, int damage, int armor) : base(name, health, damage, armor)
-        {
-            _targets = targets;
-        }
-
-        public override void Attack(List<IDamageable> targets)
-        {
-            List<IDamageable> untouchedTargets = new List<IDamageable>();
-
-            untouchedTargets.AddRange(targets);
-
-            for (int i = 0; i < _targets; i++)
-            {
-                int currentTargetIndex = UserUtils.GetRandomNumber(0, untouchedTargets.Count);
-
-                untouchedTargets[currentTargetIndex].TakeDamage(currentTargetIndex);
-                untouchedTargets.RemoveAt(currentTargetIndex);
-            }
-        }
-
-        public override void ShowType()
-        {
-            Console.WriteLine("Гранатометчик");
-            Console.WriteLine($"Количество противников на атаку: {_targets}");
-        }
-    }
-
-    class MachineGunner : Fighter
-    {
-        private int _targets;
-
-        public MachineGunner(int targets, string name, int health, int damage, int armor) : base(name, health, damage, armor)
-        {
-            _targets = targets;
-        }
-
-        public override void ShowType()
-        {
-            Console.WriteLine("Пулеметчик");
-            Console.WriteLine($"Количество противников на атаку: {_targets}");
-        }
-
-        public override void Attack(List<IDamageable> targets)
-        {
-            for (int i = 0; i < _targets; i++)
-            {
-                targets[UserUtils.GetRandomNumber(0, targets.Count)].TakeDamage(Damage);
-            }
-        }
-    }
-
-    static class UserUtils
-    {
-        private static Random s_random = new Random();
-        private static int _maxPercent = 100;
-        public static int MaxPercent => _maxPercent;
-
-        public static int GetRandomNumber(int min, int max)
-        {
-            return s_random.Next(min, max);
+            Console.WriteLine($"Имя {Name}, Кол-во жизней {_health}.");
         }
     }
 }
