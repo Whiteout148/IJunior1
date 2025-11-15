18903b383d289aa7c50ea2d567c1bfad0a1c0334
Whiteout148 <rinattemeev@gmail.com>
2025-11-15 23:21:40 +0300

    Add files via upload

--- ProgramPatients.cs
+++ ProgramPatients.cs
@@ -8,202 +8,147 @@ namespace XDproject
     {
         static void Main()
         {
-            Oceanarium oceanarium = new Oceanarium();
+            Zoo zoo = new Zoo();
 
-            oceanarium.Work();
+            zoo.Work();
         }
     }
 
-    class Oceanarium
+    class Zoo
     {
-        private Aquarium _aquarium = new Aquarium();
+        public List<Aviary> _aviaries = new List<Aviary>();
+
+        public Zoo()
+        {
+            _aviaries.Add(new Aviary());
+            _aviaries.Add(new Aviary());
+            _aviaries.Add(new Aviary());
+            _aviaries.Add(new Aviary());
+
+            AddAnimalsToAviary();
+        }
 
         public void Work()
         {
-            const string CommandAddFish = "1";
-            const string CommandRemoveFish = "2";
-            const string CommandExit = "3";
+            const string ExitCommand = "exit";
 
             bool isWork = true;
 
             while (isWork)
             {
-                Console.WriteLine("Рыбы в аквариуме: ");
-                Console.WriteLine();
-                _aquarium.ShowFisces();
-                Console.WriteLine();
-                Console.WriteLine("Типы команд:");
-                Console.WriteLine($"Добавить рыбу: {CommandAddFish}");
-                Console.WriteLine($"Забрать рыбу: {CommandRemoveFish}");
-                Console.WriteLine($"Выход: {CommandExit}");
-                _aquarium.BecomeOldFisces();
-                _aquarium.ShowDeadFisces();
+                Console.WriteLine("** Зоопарк **");
+                Console.WriteLine("\nКоличество вольеров: " + _aviaries.Count);
+                Console.WriteLine("Напишите номер вольера или (exit) для выхода.");
+                string userInput = Console.ReadLine();
 
-                switch (Console.ReadLine())
+                if (userInput == ExitCommand)
                 {
-                    case CommandAddFish:
-                        _aquarium.AddFish();
-                        break;
-
-                    case CommandRemoveFish:
-                        _aquarium.RemoveFish();
-                        break;
-
-                    case CommandExit:
-                        isWork = false;
-                        break;
-
-                    default:
-                        Console.WriteLine("Нету такой команды.");
-                        break;
+                    isWork = false;
+                }
+                else if (UserUtils.TryGetNumberInRange(userInput, out int result, _aviaries.Count))
+                {
+                    ShowAviary(result - 1);
+                }
+                else
+                {
+                    Console.WriteLine("Нету такой команды!");
                 }
 
                 Console.ReadKey();
                 Console.Clear();
             }
         }
-    }
 
-    class Aquarium
-    {
-        private List<Fish> _fisces = new List<Fish>();
-
-        public void ShowDeadFisces()
+        private void ShowAviary(int number)
         {
-            for (int i = _fisces.Count - 1; i >= 0; i--)
-            {
-                if (_fisces[i].IsDead)
-                {
-                    Console.WriteLine($"Рыбка: {_fisces[i].Name} умерла.");
-                    _fisces.RemoveAt(i);
-                }
-            }
+            Console.WriteLine("Вольер номер: " + (number + 1));
+            Console.WriteLine("Количество животных: " + _aviaries[number].AnimalsCount);
+            Console.WriteLine("Животные: ");
+
+            _aviaries[number].ShowAnimals();
         }
 
-        public void BecomeOldFisces()
+        private void AddAnimalsToAviary()
         {
-            for (int i = 0; i < _fisces.Count; i++)
+            string male = "Самец";
+            string female = "Самка";
+
+            List<Animal> animalsToAdd = new List<Animal>();
+
+            animalsToAdd.Add(new Animal("Лев", female, "Рычание"));
+            animalsToAdd.Add(new Animal("Лев", male, "Рычание"));
+            animalsToAdd.Add(new Animal("Тигр", female, "Рычание"));
+            animalsToAdd.Add(new Animal("Тигр", male, "Рычание"));
+            animalsToAdd.Add(new Animal("Свинка", male, "Хрюканье"));
+            animalsToAdd.Add(new Animal("Свинка", female, "Хрюканье"));
+
+            for (int i = 0; i < _aviaries.Count; i++)
             {
-                _fisces[i].BecomeOlder();
+                _aviaries[i].AddAnimals(UserUtils.GetRandomAnimals(animalsToAdd));
             }
-        }
-
-        public void ShowFisces()
-        {
-            for (int i = 0; i < _fisces.Count; i++)
-            {
-                _fisces[i].ShowInfo();
-            }
-        }
-
-        public void AddFish()
-        {
-            string userName = GetUserMessage("Введите название рыбы:");
-
-            if (IsContainsFish(userName))
-            {
-                Console.WriteLine("Такая рыба уже есть, выберите другое имя.");
-            }
-            else
-            {
-                string userAge = GetUserMessage("Введите возраст рыбы:");
-
-                if (UserUtils.TryGetPositiveNumber(userAge, out int resultAge))
-                {
-                    _fisces.Add(new Fish(resultAge, userName));
-                    Console.WriteLine("Рыба добавлена.");
-                }
-            }
-        }
-
-        public void RemoveFish()
-        {
-            string userName = GetUserMessage("Введите имя рыбы которую хотите забрать:");
-
-            if (IsContainsFish(userName))
-            {
-                _fisces.RemoveAt(GetIndexWithName(userName));
-                Console.WriteLine("Рыба удалена.");
-            }
-            else
-            {
-                Console.WriteLine("Такой рыбы нету!");
-            }
-        }
-
-        private string GetUserMessage(string message)
-        {
-            Console.WriteLine(message);
-            return Console.ReadLine();
-        }
-
-        private int GetIndexWithName(string userName)
-        {
-            for (int i = 0; i < _fisces.Count; i++)
-            {
-                if (_fisces[i].Name.ToLower() == userName.ToLower())
-                {
-                    return i;
-                }
-            }
-
-            return -1;
-        }
-
-        private bool IsContainsFish(string userName)
-        {
-            for (int i = 0; i < _fisces.Count; i++)
-            {
-                if (_fisces[i].Name == userName)
-                {
-                    return true;
-                }
-            }
-
-            return false;
         }
     }
 
-    class Fish
+    class Aviary
     {
-        private int _health;
+        private List<Animal> _animals = new List<Animal>();
+        public int AnimalsCount => _animals.Count;
 
-        public Fish(int health, string name)
+        public void AddAnimals(List<Animal> animalsToAd)
         {
-            _health = health;
-            Name = name;
+            _animals.AddRange(animalsToAd);
         }
 
-        public bool IsDead => _health < 0;
-
-        public string Name { get; private set; }
-
-        public void BecomeOlder()
+        public void ShowAnimals()
         {
-            _health--;
+            for (int i = 0; i < _animals.Count; i++)
+            {
+                _animals[i].ShowInfo();
+                Console.WriteLine();
+            }
+        }
+    }
+
+    class Animal
+    {
+        private string _type;
+        private string _gender;
+        private string _sound;
+
+        public Animal(string type, string gender, string sound)
+        {
+            _type = type;
+            _gender = gender;
+            _sound = sound;
         }
 
         public void ShowInfo()
         {
-            Console.WriteLine($"Имя {Name}, Кол-во жизней {_health}.");
+            Console.WriteLine($"Тип {_type} Пол {_gender} Звук {_sound}");
+        }
+
+        public Animal GetClone()
+        {
+            return new Animal(_type, _gender, _sound);
         }
     }
 
     static class UserUtils
     {
-        public static string GetUserMessage(string message)
+        private static Random s_random = new Random();
+
+        public static int GetRandomNumber(int min, int max)
         {
-            Console.WriteLine(message);
-            return Console.ReadLine();
+            return s_random.Next(min, max);
         }
-        
-        public static bool TryGetPositiveNumber(string userNumber, out int resultNumber)
+
+        public static bool TryGetNumberInRange(string userNumber, out int result, int max)
         {
-            if (int.TryParse(userNumber, out resultNumber))
+            if (int.TryParse(userNumber, out result))
             {
-                if (resultNumber <= 0)
+                if (result > max || result <= 0)
                 {
-                    Console.WriteLine("Введите число которое больше чем ноль");
+                    Console.WriteLine("Число больше чем максимальное количество!");
                 }
                 else
                 {
@@ -216,6 +161,18 @@ namespace XDproject
             }
 
             return false;
+        }
+
+        public static List<Animal> GetRandomAnimals(List<Animal> animals)
+        {
+            List<Animal> animalsToGive = new List<Animal>();
+
+            for (int i = 0; i < GetRandomNumber(1, animals.Count); i++)
+            {
+                animalsToGive.Add(animals[i].GetClone());
+            }
+
+            return animalsToGive;
         }
     }
 }
