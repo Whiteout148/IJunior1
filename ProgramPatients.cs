a5faf540f77941f4bb5ed1904725df217799254f
Whiteout148 <rinattemeev@gmail.com>
2025-12-03 23:23:37 +0300

    Add files via upload

--- ProgramPatients.cs
+++ ProgramPatients.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Media;
+using System.Runtime.InteropServices;
 
 namespace XDproject
 {
@@ -9,7 +10,7 @@ namespace XDproject
     {
         static void Main()
         {
-            InvestigativeCommittee investigativeCommittee = new InvestigativeCommittee();
+            InvestigativeCommittee investigativeCommittee = new InvestigativeCommittee("Антиправительственное");
 
             investigativeCommittee.Work();
         }
@@ -18,91 +19,60 @@ namespace XDproject
     class InvestigativeCommittee
     {
         private List<Criminal> _criminals = new List<Criminal>();
-        
-        public InvestigativeCommittee()
+        private string _crimeToLiberation;
+
+        public InvestigativeCommittee(string crimeToLiberation)
         {
-            _criminals.Add(new Criminal("Ороспу Евлады", "Турок", 170, 100, true));
-            _criminals.Add(new Criminal("Яррак кафа", "Турок", 170, 100, false));
-            _criminals.Add(new Criminal("Ахмед Амыноглу", "Турок", 180, 80, false));
-            _criminals.Add(new Criminal("Иван Сундуков", "Русский", 165, 70, true));
-            _criminals.Add(new Criminal("Владимир Буянов", "Русский", 175, 90, false));
-            _criminals.Add(new Criminal("Алексей Смирнов", "Русский", 175, 90, false));
+            _criminals.Add(new Criminal("Хлебов Антон Глебов", "Антиправительственное"));
+            _criminals.Add(new Criminal("Темеев Дмитрий Калугин", "Антиправительственное"));
+            _criminals.Add(new Criminal("Буянов Владимир Иванов", "Воровство"));
+            _criminals.Add(new Criminal("Иванов Николай Захаров", "Убийство"));
+
+            _crimeToLiberation = crimeToLiberation;
         }
 
         public void Work()
         {
-            Console.WriteLine("** Следственный комитет **");
-            Console.WriteLine("Напишите рост вес и национальность для поиска преступника.");
-            Console.WriteLine("Напишите рост:");
-            int userHeight = UserUtils.ReadInt();
-            Console.WriteLine("Напишите вес:");
-            int userWeight = UserUtils.ReadInt();
-            Console.WriteLine("Напишите национальность:");
-            string userNationality = Console.ReadLine();
+            Console.WriteLine("До амнистии");
 
-            ShowCriminalsWithIndications(userNationality, userHeight, userWeight);
+            for (int i = 0; i < _criminals.Count; i++)
+            {
+                _criminals[i].ShowInfo();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("После амнистии");
+            Console.WriteLine();
+
+            Amnesty();
         }
 
-        private void ShowCriminalsWithIndications(string nationality, int height, int weight)
+        private void Amnesty()
         {
-            var filteredCriminals = _criminals.Where(criminal => criminal.Nationality == nationality && criminal.Height == height && criminal.Weight == weight && criminal.IsInPrison == false).Select(Criminal => Criminal);
+            var filteredCriminals = _criminals.Where(criminal => criminal.Crime != _crimeToLiberation).Select(criminal => criminal);
 
-            int criminalsCount = 0;
-
-            if (criminalsCount < 1)
+            foreach (var filtered in filteredCriminals)
             {
-                Console.WriteLine("Не нашлось преступников с такими данными.");
-            }
-            else
-            {
-                Console.WriteLine("Все преступники с такими данными которые на свободе:");
-
-                foreach (var filteredCriminal in filteredCriminals)
-                {
-                    filteredCriminal.ShowInfo();
-
-                    criminalsCount++;
-                }
+                filtered.ShowInfo();
             }
         }
     }
 
     class Criminal
     {
-        public Criminal(string fullName, string nationality, int height, int weight, bool isInPrison)
+        private string _fullName;
+
+        public Criminal(string fullName, string crime)
         {
-            FullName = fullName;
-            Nationality = nationality;
-            Height = height;
-            Weight = weight;
-            IsInPrison = isInPrison;
+            _fullName = fullName;
+            Crime = crime;
         }
 
-        public string FullName { get; private set; }
-        public string Nationality { get; private set; }
-        public int Height { get; private set; }
-        public int Weight { get; private set; }
-        public bool IsInPrison { get; private set; }
+        public string Crime { get; private set; }
 
         public void ShowInfo()
         {
-            Console.WriteLine($"Имя: {FullName} Национальность: {Nationality} Рост: {Height} Вес: {Weight}");
-        }
-    }
-
-    static class UserUtils
-    {
-        public static int ReadInt()
-        {
-            string userInput = " ";
-            int result = 0;
-
-            while (int.TryParse(userInput, out result) == false)
-            {
-                userInput = Console.ReadLine();
-            }
-
-            return result;
+            Console.WriteLine($"Фио: {_fullName} Преступление: {Crime}");
         }
     }
 }
