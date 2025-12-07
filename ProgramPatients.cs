242fb7824daccd6e96f9622d7b4618b63eed0d94
Whiteout148 <rinattemeev@gmail.com>
2025-12-07 21:18:21 +0300

    Add files via upload

--- ProgramPatients.cs
+++ ProgramPatients.cs
@@ -11,77 +11,143 @@ namespace XDproject
     {
         static void Main()
         {
-            InvestigativeCommittee investigativeCommittee = new InvestigativeCommittee("Антиправительственное");
+            PsychHospital investigativeCommittee = new PsychHospital();
 
             investigativeCommittee.Work();
         }
     }
 
-    class InvestigativeCommittee
+    class PsychHospital
     {
-        private List<Criminal> _criminals = new List<Criminal>();
-        private string _crimeToLiberation;
+        private List<Patients> _patients = new List<Patients>();
 
-        public InvestigativeCommittee(string crimeToLiberation)
+        public PsychHospital()
         {
-            _criminals.Add(new Criminal("Хлебов Антон Глебов", "Антиправительственное"));
-            _criminals.Add(new Criminal("Темеев Дмитрий Калугин", "Антиправительственное"));
-            _criminals.Add(new Criminal("Буянов Владимир Иванов", "Воровство"));
-            _criminals.Add(new Criminal("Иванов Николай Захаров", "Убийство"));
-
-            _crimeToLiberation = crimeToLiberation;
+            _patients.Add(new Patients("Петров Антон Борисов", 12, "Шиза"));
+            _patients.Add(new Patients("Темеев Дмитрий Калугин", 28, "Депрессия"));
+            _patients.Add(new Patients("Буянов Владимир Иванов", 9, "Аутизм"));
+            _patients.Add(new Patients("Иванов Николай Захаров", 50, "ПТСР"));
+            _patients.Add(new Patients("Захаров Иван Антонов", 40, "ПТСР"));
+            _patients.Add(new Patients("Ибрагимов Руслан Дудаев", 21, "Шиза"));
+            _patients.Add(new Patients("Могильников Алексей Абрамов", 35, "Депрессия"));
+            _patients.Add(new Patients("Сокарев Евгений Владимиров", 30, "ОКР"));
+            _patients.Add(new Patients("Иванов Андропов Михайлов", 47, "ОКР"));
+            _patients.Add(new Patients("Дударин Иван Николаев", 7, "Аутизм"));
         }
 
         public void Work()
         {
-            Console.WriteLine("До амнистии");
-            Console.WriteLine();
+            const string SortByNameCommand = "1";
+            const string SortByAgeCommand = "2";
+            const string SearchByDiseaseCommand = "3";
+            const string ExitCommand = "4";
 
-            UserUtils.ShowCollection(_criminals);
+            bool isWork = true;
 
-            Console.WriteLine();
-            Console.WriteLine("После амнистии");
-            Console.WriteLine();
+            while (isWork)
+            {
+                Console.WriteLine("** Психушка **");
+                Console.WriteLine("Пациенты: ");
 
-            Amnesty();
+                UserUtils.ShowCollection(_patients);
 
-            UserUtils.ShowCollection(_criminals);
+                Console.WriteLine($"Команда для сортировки пациентов по ФИО: {SortByNameCommand}");
+                Console.WriteLine($"Команда для сортировки по возрасту: {SortByAgeCommand}");
+                Console.WriteLine($"Команда для поиска по заболеванию: {SearchByDiseaseCommand}");
+                Console.WriteLine($"Команда для выхода: {ExitCommand}");
+
+                switch (Console.ReadLine())
+                {
+                    case SortByNameCommand:
+                        SortByName();
+                        break;
+
+                    case SortByAgeCommand:
+                        SortByAge();
+                        break;
+
+                    case SearchByDiseaseCommand:
+                        SearchByDisease();
+                        break;
+
+                    case ExitCommand:
+                        isWork = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Такой команды нету.");
+                        break;
+                }
+
+                Console.ReadKey();
+                Console.Clear();
+            }
         }
 
-        private void Amnesty()
+        private void SortByName()
         {
-            var filteredCriminals = _criminals.Where(criminal => criminal.Crime != _crimeToLiberation).Select(criminal => criminal).ToList();
+            var orderedPatients = _patients.OrderBy(patient => patient.FullName).ToList();
 
-            _criminals = filteredCriminals;
+            UserUtils.ShowCollection(orderedPatients);
+        }
+
+        private void SortByAge()
+        {
+            var orderedPatients = _patients.OrderBy(patient => patient.Age).ToList();
+
+            UserUtils.ShowCollection(orderedPatients);
+        }
+
+        private void SearchByDisease()
+        {
+            Console.WriteLine("Введите заболевание:");
+            string userInput = Console.ReadLine();
+
+            var filteredPatients = _patients.Where(patient => patient.Disease == userInput).ToList();
+
+            if (filteredPatients.Count <= 0)
+            {
+                Console.WriteLine("Пациентов с таким заболеванием нету.");
+            }
+            else
+            {
+                UserUtils.ShowCollection(filteredPatients);
+            }
         }
     }
 
-    class Criminal
+    class Patients
     {
-        private string _fullName;
-
-        public Criminal(string fullName, string crime)
+        public Patients(string fullName, int age, string disease)
         {
-            _fullName = fullName;
-            Crime = crime;
+            FullName = fullName;
+            Age = age;
+            Disease = disease;
         }
 
-        public string Crime { get; private set; }
+        public string FullName { get; private set; }
+        public int Age { get; private set; }
+        public string Disease { get; private set; }
 
         public void ShowInfo()
         {
-            Console.WriteLine($"Фио: {_fullName} Преступление: {Crime}");
+            Console.WriteLine($"Фио: {FullName} Возраст: {Age} Заболевание: {Disease}");
         }
     }
 
     static class UserUtils
     {
-        public static void ShowCollection(List<Criminal> collection)
+        public static void ShowCollection(List<Patients> collection)
         {
+            Console.WriteLine();
+
             for (int i = 0; i < collection.Count; i++)
             {
                 collection[i].ShowInfo();
+                Console.WriteLine();
             }
+
+            Console.WriteLine();
         }
     }
 }
